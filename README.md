2d794a626b93856aec0c07e23f304a571dfdf368
Jeff Graham <jeff@funnymonkey.com>
2015-09-06 15:08:08 -0700

    Adding runtime config via webdir for app path

--- README.md
+++ README.md
@@ -4,7 +4,71 @@ This is a webserver written in go with [IndieWeb](http://indiewebcamp.com) princ
 
 Currently this does not do much other than serve some static templated files.
 
-## Current Roadmap 
+## Running
+
+After downloading and compiling the app. You can run the binary by passing it a `webdir` parameter on the commandline. eg. `indigo -webdir=/srv/www` in the webdir directory the following subdirectories are expected; 'templates', 'static', 'content'. Currently templates are hardcoded as page.html and header.html (this portion will be refactored at some point).
+
+### templates directory
+
+As noted above the templates directory only supports page.html and header.html.
+
+#### page.html
+
+````
+{{define "page"}}
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>{{template "title"}}</title>
+  <link rel="stylesheet" href="/static/css/foundation.css">
+  <link rel="stylesheet" href="/static/foundation-icons/foundation-icons.css">
+
+  <!-- This is how you would link your custom stylesheet -->
+  <link rel="stylesheet" href="/static/css/app.css">
+
+  <script src="/static/js/vendor/modernizr.js"></script>
+
+</head>
+<body>
+  <script src="/static/js/vendor/jquery.js"></script>
+  <script src="/static/js/foundation/foundation.js"></script>
+  {{template "header"}}
+  {{template "body"}}
+
+  <script>$(document).foundation();</script>
+</body>
+</html>
+{{end}}
+````
+
+#### header.html
+````
+{{define "header"}}
+<div class="icon-bar one-up">
+  <a href="http://jgraham909.com/" class="item"><i class="fi-home"></i><label>Home</label></a>
+</div>
+{{end}}
+````
+
+### static directory
+
+Any file located under static will automatically be served as a static file when accessed as http://example.com/static/PATH-TO-STATIC-FROM-STATIC-DIR
+
+### content directory
+
+Intended to be served as golang templates. Note that any template values used in the templates directory can be used. As an example if using the example page.html and header.html templates from above then you can make a post by naming it how you want to access it. For example if I wanted a blog post at http://example.com/blog/article-1 I would make a directory `blog` inside my content directory and then a corresponding file called `artile-1` (note no file extension). As an example here is a possible template for article-1;
+
+````
+{{define "title"}}example.com: Article 1{{end}}
+
+{{define "body"}}
+This is my first blog post! Woohoo!
+{{end}}
+
+````
+
+## Current Roadmap
 
 1. Add capability to add notes, articles, and replies
 2. Add webmention functionality
--- indigo.go
+++ indigo.go
@@ -1,72 +1,85 @@
 package main
 
 import (
-  "html/template"
-  "log"
-  "net/http"
-  "os"
-  "path"
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path"
 )
 
-const webDir = "/root/static/"
-const templateDir = webDir + "templates/"
-const contentDir = webDir + "content/"
-const staticDir = webDir + "static/"
+var config struct {
+	webDir      string
+	templateDir string
+	contentDir  string
+	staticDir   string
+}
 
 func main() {
-  serveSingle("/favicon.ico", staticDir + "/favicon.ico")
-  fs := http.FileServer(http.Dir(staticDir))
-  http.Handle("/static/", http.StripPrefix("/static", fs))
-  http.HandleFunc("/", serveTemplate)
+	setConfig()
+	serveSingle("/favicon.ico", config.staticDir+"/favicon.ico")
+	fs := http.FileServer(http.Dir(config.staticDir))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+	http.HandleFunc("/", serveTemplate)
 
-  log.Println("Listening...")
-  http.ListenAndServe(":80", nil)
+	log.Println("Listening...")
+	http.ListenAndServe(":80", nil)
+}
+
+func setConfig() {
+	config.webDir = "/srv/www/"
+	flag.StringVar(&config.webDir, "webdir", config.webDir, "Root of website templates and static content")
+	flag.Parse()
+
+	config.templateDir = config.webDir + "templates"
+	config.contentDir = config.webDir + "content"
+	config.staticDir = config.webDir + "static"
 }
 
 func serveTemplate(w http.ResponseWriter, r *http.Request) {
-  var fp string;
-  pp := path.Join(templateDir, "page.html")
-  hp := path.Join(templateDir, "header.html")
-  if (r.URL.Path == "/") {
-    fp = path.Join(contentDir, "/index.html")
-  } else {
-    fp = path.Join(contentDir, r.URL.Path)
-  }
-  // Return a 404 if the template doesn't exist
-  info, err := os.Stat(fp)
-  if err != nil {
-    if os.IsNotExist(err) {
-      log.Println(err.Error())
-      http.NotFound(w, r)
-      return
-    }
-  }
+	var fp string
+	pp := path.Join(config.templateDir, "page.html")
+	hp := path.Join(config.templateDir, "header.html")
+	if r.URL.Path == "/" {
+		fp = path.Join(config.contentDir, "/index.html")
+	} else {
+		fp = path.Join(config.contentDir, r.URL.Path)
+	}
+	// Return a 404 if the template doesn't exist
+	info, err := os.Stat(fp)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Println(err.Error())
+			http.NotFound(w, r)
+			return
+		}
+	}
 
+	// Return a 404 if the request is for a directory
+	if info.IsDir() {
+		log.Println("Directory listing requested")
+		http.NotFound(w, r)
+		return
+	}
 
-  // Return a 404 if the request is for a directory
-  if info.IsDir() {
-    log.Println("Directory listing requested")
-    http.NotFound(w, r)
-    return
-  }
+	tmpl, err := template.ParseFiles(pp, hp, fp)
+	if err != nil {
+		// Log the detailed error
+		log.Println(err.Error())
+		// Return a generic "Internal Server Error" message
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
 
-  tmpl, err := template.ParseFiles(pp, hp, fp)
-  if err != nil {
-    // Log the detailed error
-    log.Println(err.Error())
-    // Return a generic "Internal Server Error" message
-    http.Error(w, http.StatusText(500), 500)
-    return
-  }
-
-  if err := tmpl.ExecuteTemplate(w, "page", nil); err != nil {
-    log.Println(err.Error())
-    http.Error(w, http.StatusText(500), 500)
-  }
+	if err := tmpl.ExecuteTemplate(w, "page", nil); err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	}
 }
 
 func serveSingle(pattern string, filename string) {
-    http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, filename)
-    })
+	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filename)
+	})
 }
